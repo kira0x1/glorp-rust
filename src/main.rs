aa81f1934fd57dbadb0071836c2928a166e5ccb1
Kira <kira.eva@protonmail.com>
2026-02-05 20:35:21 +0000

    add routes

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use axum::{Router, routing::get};
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }

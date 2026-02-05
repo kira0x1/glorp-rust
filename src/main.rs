41b4298208e627d8018ea81bd280abfe2f578063
Kira <kira.eva@protonmail.com>
2026-02-05 20:46:10 +0000

    implement askama

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod templater;
+
+use askama::Template;
 use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let hello = templater::HelloTemplate { name: "meow" };
+    let app = Router::new().route("/", get(hello.render().unwrap()));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
---
+++ src/templater.rs
@@ -0,0 +1,7 @@
+﻿use askama::Template;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+pub struct HelloTemplate<'a> {
+    pub(crate) name: &'a str,
+}
